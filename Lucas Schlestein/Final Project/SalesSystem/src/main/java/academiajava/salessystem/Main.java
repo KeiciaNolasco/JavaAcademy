ae02c9542212a9694d6bf12e34a3ebcde73ae57a
Keicia Nolasco <129958859+KeiciaNolasco@users.noreply.github.com>
2024-06-11 15:24:51 -0500

    Final Project Update
    Final Project Update - Class 09 - Teacher Lucas Schlestein

--- Lucas Schlestein/Final Project/SalesSystem/src/main/java/academiajava/salessystem/Main.java
+++ Lucas Schlestein/Final Project/SalesSystem/src/main/java/academiajava/salessystem/Main.java
@@ -6,19 +6,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        // Configurando o formatador de data
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-        ShoppingCart car1 = new ShoppingCart();
-        car1.addProduct(new Electronic("Television", 7000.00, 1, 12));
-        car1.addProduct(new Clothing("T-Shirt", 150.00, 1, "large", "blue"));
-        car1.addProduct(new Food("Rice", 15.30, 1, LocalDate.parse("02/06/2025", formatter)));
+        // Inicializando carrinhos de compras com produtos
+        ShoppingCart cart1 = new ShoppingCart();
+        cart1.addProduct(new Electronic("Television", 5000.00, 1, 12));
+        cart1.addProduct(new Clothing("T-Shirt", 150.00, 1, "large", "blue"));
+        cart1.addProduct(new Food("Rice", 15.30, 1, LocalDate.parse("02/06/2025", formatter)));
 
-        ShoppingCart car2 = new ShoppingCart();
-        car2.addProduct(new Electronic("Computer", 3000.00, 2, 24));
-        car2.addProduct(new Clothing("Socks", 20.00, 4, "Average", "black"));
-        car2.addProduct(new Food("Tomato Sauce", 10.50, 2, LocalDate.parse("05/10/2025", formatter)));
-
+        ShoppingCart cart2 = new ShoppingCart();
+        cart2.addProduct(new Electronic("Computer", 3000.00, 2, 24));
+        cart2.addProduct(new Clothing("Socks", 20.00, 4, "Average", "black"));
+        cart2.addProduct(new Food("Tomato Sauce", 10.50, 2, LocalDate.parse("05/10/2025", formatter)));
 
         Scanner scanner = new Scanner(System.in);
 
@@ -29,39 +29,51 @@ public class Main {
                 System.out.print("\nSelect the option you want" +
                         "\n1- View shopping cart 1;" +
                         "\n2- View shopping cart 2;" +
-                        "\n3- Save shopping carts in a \".txt\" file." +
+                        "\n3- Add new Product;" +
+                        "\n4- Remove a product;" +
+                        "\n5- Save shopping carts in a \".txt\" file;" +
+                        "\n6- Log out of the system." +
                         "\nEnter the item number: ");
                 int item = Integer.parseInt(scanner.nextLine());
 
-                    if (item == 1) {
+                switch (item) {
+                    case 1:
                         System.out.println("\nShopping Cart 1:");
-                        car1.listProduct();
-                        System.out.println("Total: " + car1.calculateTotal());
-                    }
-                    if (item == 2) {
+                        cart1.listProduct();
+                        System.out.println("Total: " + cart1.calculateTotal());
+                        break;
+                    case 2:
                         System.out.println("\nShopping Cart 2:");
-                        car2.listProduct();
-                        System.out.println("Total: " + car2.calculateTotal());
-                    }
-                    if (item == 3) {
+                        cart2.listProduct();
+                        System.out.println("Total: " + cart2.calculateTotal());
+                        break;
+                    case 3:
+                        System.out.print("\nOption not yet available in the system!");
+                        break;
+                    case 4:
+                        System.out.print("\nOption not yet available in the system!");
+                        break;
+                    case 5:
                         try {
-                            car1.generateTextFile("ShoppingCart1.txt");
-                            car2.generateTextFile("ShoppingCart2.txt");
-                            System.out.println("\nText files created successfully!" + "\nYou can find it in the project root folder.");
+                            cart1.generateTextFile("ShoppingCart1.txt");
+                            cart2.generateTextFile("ShoppingCart2.txt");
+                            System.out.println("\nText files created successfully!" + "\nYou can find them in the project root folder.");
                         } catch (ShoppingCartException e) {
                             System.err.println(e.getMessage());
                             e.printStackTrace();
                         }
-                    }
-                    break;
+                        break;
+                    case 6:
+                        System.out.println("\nLogging out...");
+                        scanner.close();
+                        System.exit(0);
+                    default:
+                        System.out.println("\nInvalid option! Please enter a valid number.");
+                        break;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("\nThe value entered does not correspond to a number!");
             }
-            catch(Exception e){
-                    System.out.println("\nThe value entered does not correspond to a number!");
-            }
-
         }
-
-        scanner.close();
-
     }
 }
